76fa1e3d7096c887cc4f1326021a60f2a85a5a22
yunmengzhizhi <110776633+yunmengzhizhi@users.noreply.github.com>
2022-08-08 22:41:49 +0800

    added a file

---
+++ Department.java
@@ -0,0 +1,16 @@
+package cn.wolfcode.rbac.domain;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Department {
+    private Long id;
+    private String name;
+    private String sn;
+
+}
